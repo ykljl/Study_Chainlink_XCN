08447a0eb5fff4dcc9674c05dc6cad4ab9e6ea6f
VSC_ljl <1756858117@qq.com>
2025-05-04 12:21:46 +0800

    chore: ignore dependencies and cache folders

---
+++ Study_Chainlink_cn_5/node_modules/@zksync/contracts/l1-contracts/contracts/common/Dependencies.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+/* solhint-disable-next-line no-unused-import */
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+/* solhint-disable-next-line no-unused-import */
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
--- Study_Chainlink_cn_5/prettier.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: ["prettier-plugin-solidity"],
-  // 你可以在这里添加其他的 Prettier 配置选项
-  tabWidth: 2, // 设置缩进宽度为 2 个空格
-  singleQuote: true, // 使用单引号代替双引号
-};
